9e5a755f0b335ac47da6d6c99966a8435398cc2c
naveen bansal <bansalnvn@gmail.com>
2019-01-26 13:56:18 +0800

    initial commit

---
+++ desktop/src/com/daddy/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.daddy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.daddy.StarFishGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarFishGame(), config);
+	}
+}
